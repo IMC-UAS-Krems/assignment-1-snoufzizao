4f8126ed347dbf905322b22848a7bd26bca88aaf
snoufzizao <25IMC10138@imc.ac.at>
2026-03-29 16:11:29 +0200

    class fix with test

--- src/streaming/artists.py
+++ src/streaming/artists.py
@@ -7,6 +7,9 @@ Classes to implement:
   - Artist
 """
 
+from streaming.albums import Album
+
+
 class Artist:  #musician or content creator, separate?
     def __init__(self, name, genre):
         self.name = name
@@ -15,6 +18,11 @@ class Artist:  #musician or content creator, separate?
 
     def add_album(self, album):
         self.albums.append(album)
+
+    def single_release(self, title, duration, genre=None, release_date=None):
+        single = Album(title, self.name, genre=genre, release_date=release_date)
+        self.add_album(single)
+        return single
 
     def __str__(self): 
         album_list = "\n".join([f"{i+1}. {album.title}" for i, album in enumerate(self.albums)])
--- src/streaming/users.py
+++ src/streaming/users.py
@@ -10,3 +10,50 @@ Classes to implement:
     - FamilyAccountUser
     - FamilyMember
 """
+class User():
+    def __init__(self, username, email, age=None):
+        self.username = username
+        self.email = email
+        self.age = age
+
+    def __str__(self):
+        return f"User: {self.username} ({self.email})"
+    
+class FreeUser(User):
+    def __init__(self, username, email, age=None):
+        super().__init__(username, email, age)
+        self.subscription_type = "Free"
+    
+    def __str__(self):
+        return f"Free User: {self.username} ({self.email})"
+  
+
+class PremiumUser(User):
+    def __init__(self, username, email, age=None, subscription_start=None):
+        super().__init__(username, email, age)
+        self.subscription_type = "Premium"
+        self.subscription_start = subscription_start
+
+    def __str__(self):
+        return f"Premium User: {self.username} ({self.email})"
+
+class FamilyAccountUser(User):
+    def __init__(self, username, email, age=None):
+        super().__init__(username, email, age)
+        self.subscription_type = "Family"
+        self.members = []
+
+    def NewMembers(self, username, email, age=None):
+        self.members.append(FamilyMember(username, email, age))
+
+    def __str__(self):
+        return f"Family Account User: {self.username} ({self.email})"
+    
+class FamilyMember(User):
+    def __init__(self, username, email, age=None, parent=None):
+        super().__init__(username, email, age)
+        self.subscription_type = "Family User"
+        self.parent = parent
+
+    def __str__(self):
+        return f" Family Member: {self.username} ({self.email})"
